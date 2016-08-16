eaac09af49998b3b4657a15b99e495ae7333ef63
hillarynnamdi <hillarynnamdievans@gmail.com>
2016-08-16 17:57:46 +0100

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160812132157) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160812132157) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.string   "unconfirmed_email"
   end
 
   add_index "admins", ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true, using: :btree
