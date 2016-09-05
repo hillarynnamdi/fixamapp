74e0db372d913264443bbf5b74e3ac91097af4d8
chrisgeek <chrisgeek29@gmail.com>
2016-09-05 17:42:36 +0100

    default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.assets.digest = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default_url_options = { :host => "gofixam.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "gofixam.com" }
   config.action_mailer.delivery_method = :smtp
 
 
