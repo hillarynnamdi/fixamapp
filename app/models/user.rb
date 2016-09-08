6bb003370d1c0a9677e9812a58ded6a163f4f434
hillarynnamdi <hillarynnamdievans@gmail.com>
2016-09-08 18:12:58 +0100

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,10 @@ class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
 
-		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-		user.email = auth.info.email
+		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+  		user.email = auth.info.email
 	    user.first_name=auth.info.first_name
 	    user.last_name=auth.info.last_name
 	    user.gender = auth.extra.raw_info.gender
