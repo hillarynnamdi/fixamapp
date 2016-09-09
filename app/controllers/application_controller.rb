bc24928da20d11d5497a7fbc92223edf3a68dd9d
hillarynnamdi <hillarynnamdievans@gmail.com>
2016-09-09 13:27:26 +0100

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   add_flash_types :successful
-#  before_action :configure_permitted_parameters, if: :devise_controller?
+ before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
     def after_sign_in_path_for(resource_or_scope)
