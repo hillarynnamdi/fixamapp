95c90254503d837d5d1846a0ae09f97c97ca0a98
chrisgeek <chrisgeek29@gmail.com>
2016-10-10 19:57:20 +0100

    sitemap

--- db/migrate/20160703204204_add_more_details_to_users.rb
+++ db/migrate/20160703204204_add_more_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddMoreDetailsToUsers < ActiveRecord::Migration
   def change
-  	add_column :users, :first_name, :string	, null: false, default: ""
+  	add_column :users, :first_name, :string	, null: false, default: "" 
     add_column :users, :last_name, :string	, null: false, default: ""
     add_column :users, :gender, :string	, null: false, default: ""
     add_column :users, :phone_number, :string	, null: false, default: ""
