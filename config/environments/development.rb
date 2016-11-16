dd13c42a3fa289cc984af8364a15a186d866eaa7
chrisgeek <chrisgeek29@gmail.com>
2016-11-16 11:26:41 +0100

    mail sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,16 +21,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "mail.google.com",
-    user_name: "chrisgeeq@gmail.com" ,
-     password:  "ifeanyi29" ,
-    :authentication => "plain",
-   :enable_starttls_auto => true
-
- } 
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :domain => 'localhost:3000',
+  :user_name =>  ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+}
 
   config.action_mailer.default_url_options={host: "localhost:3000"}
 
