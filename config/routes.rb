5f73fc1efbd77c36a212e2c8b1da0911c6846cf5
hillarynnamdi <hillarynnamdievans@gmail.com>
2016-11-18 20:08:08 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
     end
 end
+
+resources :terms,path: '/terms_and_conditions'
+
       get "/404", :to => "errors#not_found"
       get "/422", :to => "errors#unacceptable"
       get "/500", :to => "errors#internal_error"
