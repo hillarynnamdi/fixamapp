da28ea2e698c635467a0884ba0ac3d3ff62a1d67
hillarynnamdi <hillarynnamdievans@gmail.com>
2016-12-04 00:23:17 +0100

    commit

--- app/assets/javascripts/fixam.js
+++ app/assets/javascripts/fixam.js
@@ -7,7 +7,15 @@ $(document).ready(
   })
 }
 )
-
+$(document).ready(
+  function(){
+  $('#desktopmenu a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top;
+  $('body,html').animate({scrollTop:posi},700);
+  return false;
+});
+  })
 
 
 $(document).ready(
