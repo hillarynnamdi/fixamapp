47858f2020adcc18d3e5424ff8278d00f1a2650a
Hillary Nnamdi Evans <hillarynnamdievans@gmail.com>
2017-02-01 13:11:52 -0800

    blog

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,5 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable,:registerable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 end
